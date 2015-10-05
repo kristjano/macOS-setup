39261525ffb7cd148d2ff84512d125298d2a0126
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2015-10-05 12:39:27 +0000

    Updated comment in zsh.sh
    zsh.sh only changes the default shell for your user

--- zsh.sh
+++ zsh.sh
@@ -3,6 +3,6 @@
 # add latest zsh to list of standard shells
 echo "/usr/local/bin/zsh" >> /etc/shells
 
-# set zsh as default shell
+# set zsh as default shell for current user
 sudo -u ${SUDO_USER} chsh -s /usr/local/bin/zsh
 
