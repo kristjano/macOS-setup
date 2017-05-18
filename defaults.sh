15105a5f6fb1dcc16fceddc6f4cd37c90883d52a
Kristján Ólafur Ólafsson <kristjanola13@ru.is>
2017-05-18 22:30:52 +0000

    Added to defaults for App Store to update daily
    App Store checks for app updates daily.

--- defaults.sh
+++ defaults.sh
@@ -6,3 +6,5 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 0
 
+# Set App Store to check for updates daily
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
